4fa5b2812bf1c2630324c76b79938cc359d91e3a
Nexus Jr <contato@nexusjr.com>
2018-03-15 22:27:57 -0300

    Mudanças php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,10 +17,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
 $to = 'contato@nexusjr.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$email_subject = "Contato do Site:  $name";
+$email_body = "Você recebeu uma nova mensagem do formulário de contato do seu site.\n\n"."Aqui estão os detalhes:\n\nName: $name\n\nEmail: $email_address\n\nCelular: $phone\n\nMensagem:\n$message";
+$headers = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Responda para: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
